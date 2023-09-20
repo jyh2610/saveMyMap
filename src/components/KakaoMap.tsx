5558b764bef2af85af3e09ffbf82a87f89d0585b
jyh2610 <jogg4177@naver.com>
2023-09-20 16:12:43 +0900

    feature : load kakaomap hooks

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,13 +1,25 @@
 "use client";
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { Map, useKakaoLoader } from "react-kakao-maps-sdk";
 
 function Kakao() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [loading, error] = useKakaoLoader({
+    appkey: process.env.NEXT_PUBLIC_RESTKEY!,
+  });
   return (
-    <Map center={{ lat: 33.5563, lng: 126.79581 }} style={{ width: "500px", height: "360px" }}>
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
-      </MapMarker>
-    </Map>
+    <Map // 지도를 표시할 Container
+      center={{
+        // 지도의 중심좌표
+        lat: 33.450701,
+        lng: 126.570667,
+      }}
+      style={{
+        // 지도의 크기
+        width: "100%",
+        height: "100vh",
+      }}
+      level={3} // 지도의 확대 레벨
+    />
   );
 }
 
